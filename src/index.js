0ab18b2f882e7f34466f90796342af9f33f14862
zzayanna <61212421+zzayanna@users.noreply.github.com>
2020-10-03 20:00:15 +0300

    Аeat: implemented max, min and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  }
+  let min = array[0];
+  for (let i = 1; i < array.length; ++i) {
+      if (array[i] < min) {
+        min = array[i];
+      }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length == 0) {
+    return 0;
+  }
+  let max = array[0];
+  for (let i = 1; i < array.length; ++i) {
+      if (array[i] > max) {
+        max = array[i];
+      }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length == 0) {
+    return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; ++i) {
+    sum += array[i];
+  }
+  return sum / array.length;
 }
+
